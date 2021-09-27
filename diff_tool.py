d445cf73312ae5a3211f6f53449bfa31e8de1a64
masedar ubuntu <mailspecialforspam@mail.ru>
2021-09-27 11:27:50 +0300

    add recursive includes files check (from second path in first path)

--- diff_tool.py
+++ diff_tool.py
@@ -7,50 +7,20 @@ import glob
 from pathlib import Path
 import os
 
-def script(): 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-first", help="full path to sources files")
-    parser.add_argument("-second", help="full path to other files")
-    parser.add_argument("-html", help="specify html to write to")
-    parser.add_argument('-c', help="use context for output")
-    parser.add_argument('-mode', help="search name by name with equal directory [use only from a dir containing other dirs] (a/b.js != b/b.js)")
-    # parser.add_argument('-cli', help="choose mode cli: web/model/react")
-    args = parser.parse_args()
-
-    firstpath = args.first  
-    secondpath = args.second
-    mode = args.mode
-    context = args.c
-
+# firstpath - source path, where takes files
+# secondpath - second path, where finds equals name files
+# context - if true write only context diff (not all file)  
+# delta - diff variable, where kept all diffs
+# args - start arguments
+# output_file - path to output file (with .html)
+# reverse - check files on presence from secondpath in firstpath  
+def findDiff(firstpath, secondpath, context, delta, args, output_file, reverse):
+    
     NotFound = '/home/masedar/NotFound.txt'
-    DiffFile = '/home/masedar/diff.html'
 
-
-
-    # default
-    if args.first:
-        firstpath = args.first    
-    else: 
-        print("Write first path (-first ~/*) or use -h for more information")
-        return
-    if args.second:
-        secondpath = args.second
-    else:   
-        print("Write second path (-second ~/*)")
-        return
-
-
-    if args.html:
-        output_file = Path(args.html)
-        print (output_file)
-    else:
-        output_file = Path(DiffFile)
-
-    delta = difflib.HtmlDiff().make_file('', '')
-    # Проход по всем файлам
+    # Loop through all files
     # files = glob.glob(firstpath + '/**/*', recursive=True)
     # print(files)
-
     for file in glob.iglob(firstpath + '/**/*.*', recursive=True):
         old_file = Path(file)
         print(old_file.name)
@@ -68,8 +38,8 @@ def script():
                             new_file = Path(new_file_path)
                             break
                     else:
-                        print(firstpath + " has files")
-                        return
+                        print(firstpath + "has files")
+                        return 1
                 else: 
                     new_file = Path(new_file_path)
         
@@ -77,18 +47,65 @@ def script():
         if new_file == None:
             with open(NotFound, "w") as f:
                 f.write("Not found. it's new file!!!\n")
-            
             file_2 = open(NotFound).readlines()
         else:
+            if reverse != 0:
+                continue
             file_2 = open(new_file).readlines()
         if context:
-            delta += difflib.HtmlDiff().make_file(file_1, file_2, old_file, new_file, True)
+            delta += difflib.HtmlDiff().make_file(file_1, file_2, old_file, new_file, True) if reverse == 0 else difflib.HtmlDiff().make_file(file_2, file_1, new_file, old_file, True)
         else :
-            delta += difflib.HtmlDiff().make_file(file_1, file_2, old_file, new_file)
+            delta += difflib.HtmlDiff().make_file(file_1, file_2, old_file, new_file) if reverse == 0 else difflib.HtmlDiff().make_file(file_2, file_1, new_file, old_file)
 
         # Записывает ответ
         with open(output_file, "w") as f:
             f.write(delta)
+    return delta
+
+def script(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-first", help="full path to sources files")
+    parser.add_argument("-second", help="full path to other files")
+    parser.add_argument("-html", help="specify html to write to")
+    parser.add_argument('-c', help="use context for output")
+    parser.add_argument('-mode', help="search name by name with equal directory [use only from a dir containing other dirs] (a/b.js != b/b.js)")
+    # parser.add_argument('-cli', help="choose mode cli: web/model/react")
+    args = parser.parse_args()
+
+    firstpath = args.first  
+    secondpath = args.second
+    mode = args.mode
+    context = args.c
+
+    DiffFile = '/home/masedar/diff.html'
+
+    # default
+    if args.first:
+        firstpath = args.first    
+    else: 
+        print("Write first path (-first ~/*) or use -h for more information")
+        return
+    if args.second:
+        secondpath = args.second
+    else:   
+        print("Write second path (-second ~/*)")
+        return
+
+    if args.html:
+        output_file = Path(args.html)
+        print (output_file)
+    else:
+        output_file = Path(DiffFile)
+
+    delta = difflib.HtmlDiff().make_file('', '')
+    
+    res = findDiff(firstpath, secondpath, context, delta, args, output_file, 0)
+    if res == 1:
+        return
+    # delta = res since give delta value and not link
+    res = findDiff(secondpath, firstpath, context, res, args, output_file, 1)
+    if res == 1:
+        return
 
     print("-------END----------")
     print ("Context enabled" if context else "Context disabled")
