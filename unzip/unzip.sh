833cd4042f705ab2233bda0c33fc753986e3c200
vm4tech <mailspecialforspam@mail.ru>
2023-08-31 14:13:51 +0300

    create custom extension for unzip

--- unzip/unzip.sh
+++ unzip/unzip.sh
@@ -7,28 +7,42 @@
 # https://stackoverflow.com/questions/60928098/how-do-i-assign-filename-to-a-variable-after-unzipping-unknown-zip-file-in-bash
 # https://unix.stackexchange.com/questions/382390/unzip-archive-with-single-file-and-rename-output-to-match-archive-name
 
-# if [[ "$path" != "" ]]; then
-#     PATH_IN_ZIP="$path"
-# else
-#     PATH_IN_ZIP="data"
-# fi
+if [[ "$1" != "" ]]; then
+    PATH_TO_ZIP=$1;
+else
+    PATH_TO_ZIP="*";
+fi
+if [[ "$2" != "" ]]; then
+    PATH_IN_ZIP=$2;
+else
+    PATH_IN_ZIP="*";
+fi
+
+if [[ "$3" != "" ]]; then
+    FILE_EXTENSION=$3;
+else
+    FILE_EXTENSION=".*";
+fi
+
+
+echo "PATH_IN_ZIP: $PATH_IN_ZIP";
+echo "PATH_TO_ZIP: $PATH_TO_ZIP";
 
 # enable ** for recursion subfolder from ./ (https://askubuntu.com/questions/1287093/bash-recursive-command-to-include-files-of-current-directory-files-as-wel)
 shopt -s globstar;
 for z in **/*.zip; do
     # unzip only data/* folder from zip
-    for i in $(unzip $z "data/*"); do
+    for i in $(unzip $z "*$FILE_EXTENSION" -d data); do
       s=${i##*/};
       zNoFolder=${z##*/};
-      if [[ $s =~ \.xml$ ]]; then
-        echo "file: ${i##*/}";
-        echo "file name: ${s%.*}";
-        echo "path to file: $i"; 
-        echo "path to zip: $z"; 
-        echo "zip: $zNoFolder"; 
-        echo "extension: ${i#*.}";
-
-        mv "data/${i##*/}" "data/${s%.*}[${zNoFolder%%.*}].xml";
+      echo "file: ${i##*/}";    
+      echo "file name: ${s%.*}";
+      echo "path to file: $i"; 
+      echo "path to zip: $z"; 
+      echo "zip: $zNoFolder"; 
+      echo "extension: ${i#*.}";
+      if [[ $s =~ $FILE_EXTENSION ]]; then
+        mv "data/${i##*/}" "data/${s%.*}[${zNoFolder%%.*}]$FILE_EXTENSION";
         echo "successed unzip file $s";
         echo "                                                           "; 
         echo "                                                           "; 
